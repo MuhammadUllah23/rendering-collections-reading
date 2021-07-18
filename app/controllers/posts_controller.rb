84441b4e32091eac07f7d2987de08629bba017d1
Muhammad <muhammadhullah23@gmail.com>
2021-07-18 17:12:37 -0400

    commit tree a41f9e03cc4c71c076456fb0f5b0521db3c84138

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,12 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:show, :edit, :update]
+	
 
 	def index
 		@posts = Post.all
 	end
 
 	def show
+		set_post
 	end
 
 	def new
@@ -19,7 +20,7 @@ class PostsController < ApplicationController
     @post.author_id = @author.id
 
 	  @post.save
-	  redirect_to post_path(@post)
+	  redirect_to posts_path(@post)
 	end
 
 	def edit
@@ -27,7 +28,7 @@ class PostsController < ApplicationController
 
 	def update
 	  @post.update(post_params)
-	  redirect_to post_path(@post)
+	  redirect_to posts_path(@post)
 	end
 
 private
